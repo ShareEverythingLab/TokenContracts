a8011808b2875fa3ba74b1427a0aa931641867fc
Max <max.ma@cozystay.com>
2018-07-20 13:55:43 -0700

    Add mint token script

--- config/address-config.js
+++ config/address-config.js
@@ -1,3 +1,4 @@
 module.exports = {
     ownerAddress: '',
+    tokenAddress: '',
 }
---
+++ scripts/mint_token.js
@@ -0,0 +1,25 @@
+const generalSettings = require('../config/general-settings');
+const addressConfig = require('../config/address-config.js');
+const gasConfig = require('../config/gas-config');
+const utils = require('../utils/utils');
+
+const web3 = utils.getWeb3(generalSettings.host);
+
+// instantiate contract
+var TokenInstance = utils.getTokenContract(
+  web3,
+  addressConfig.tokenAddress
+);
+
+// mintPredefinedTokens
+web3.eth.defaultAccount = addressConfig.ownerAddress;
+const mintingConfig = require('../config/minting-config')
+
+for (let i = 0; i < mintingConfig.length; i++) {
+    const txHash = TokenInstance.mint(
+        mintingConfig[i].address, 
+        utils.convertEthToWei(mintingConfig[i].tokenAmountETH)
+    );
+}
+
+console.log(`txHash: ${txHash}`);
